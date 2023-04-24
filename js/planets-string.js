a24168d4697e0761670a87f06b5c31ce5718e340
Emilia Mata <mataemilia2609@gmail.com>
2023-04-24 17:21:39 -0600

    Completed Planets Strings to split join

--- js/planets-string.js
+++ js/planets-string.js
@@ -10,18 +10,43 @@
 	 * planetsArray.
 	 * console.log planetsArray to check your work
 	 */
-		var planetsArray = ("Mercury", "Venus", "EarthMars", "Jupiter",Saturn|Uranus|Neptune"
-	// console.log(planetsArray);
+	
+	var planets = ["Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"];
+	var planetsAsString = planets.join("|");
+	console.log(planetsAsString);
+	
 	
 	/**
 	 * TODO:
-	 * Create a string with <br> tags between each planet. console.log() your
+	 * Create a string with <br> tags between each planet. con
+	 * sole.log() your
 	 * results. Why might this be useful?
 	 *
 	 * BONUS:
-	 * Create another string that would display your planets in an undordered
+	 * Create another string that would display your planets in an unordered
 	 * list. You will need an opening AND closing <ul> tags around the entire
 	 * string, and <li> tags around each planet.
 	 */
+	
+	// var listPlanets = "<ul>" + "<li>" + planetsString.split("|") + "</li>" + "</ul>";
+	
+	var listPlanets = planetsString.split("|");
+	var brlistPlanets = "<ul>" + "<li>" + listPlanets.join("</li><li>") + "</li>" + "</ul>";
+	console.log(listPlanets);
+	console.log(brlistPlanets);
+	
+	
+	var data = ["Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune" ];
+	
+	var listPlanets = document.getElementById("listPlanets");
+	
+	data.forEach((listPlanets)=>{
+		let li = document.createElement("li");
+		li.innerText = listPlanets;
+		list.appendChild(li);
+	
+	})
+	
+	
 	
 })();
