2bfa098b29337852b96e19aa516ea5a336dec09b
Emilia Mata <mataemilia2609@gmail.com>
2023-05-02 08:41:10 -0600

    May 2nd warmup

--- js/warmups.js
+++ js/warmups.js
@@ -8,7 +8,8 @@ function seven() {
 console.log(seven());
 console.log(seven());
 
-//2. Write a function that takes in a number and returns an array of numbers that are divisible within the argument. If the value provided is not a number, the function should return false.
+//2. Write a function that takes in a number and returns an array of numbers that are divisible within the argument.
+// If the value provided is not a number, the function should return false.
 
 function findFactors(num) {
 	if (typeof num === "number" && !isNaN(num)) {
@@ -30,5 +31,31 @@ console.log(findFactors(0)); // returns [];
 console.log(findFactors(true)); // returns false;
 console.log(findFactors("13")); // returns false;
 console.log(findFactors([54, 72, 144])); // returns false;
-console.log(findFactors({value: 64})); // returns false;
 console.log(findFactors()); // returns false;
+
+//3. May 2.
+// Write a function that takes in a string and counts the instances of the letter 'e' within the argument.
+// The count should be case insensitive. If the string does not contain any'e's it should return 0. If the
+// argument is not a string it should return false.
+
+function countEs(str) {
+	if	(typeof str === "string") {
+
+		let count = 0;
+		let normalizedStr = str.toLowerCase()
+		for (let i = 0; i < str.length; i++) {
+			if (normalizedStr.charAt(i) === "e") {
+				count++;
+			}
+		}
+		return count;
+	}
+}
+
+
+console.log(countEs("Ease")); // returns 2;
+console.log(countEs("teleconference")); // returns 5;
+console.log(countEs("TOM")); // returns 0;
+console.log(countEs(true)); // returns false;
+console.log(countEs(['e', 'E'])); // returns false;
+console.log(countEs()); // returns false;
